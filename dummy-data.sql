61f1f634f5cf521acfe7c3ce0bb45a27f8f6accb
Bhisma Bakhai <bhisma93@gmail.com>
2018-06-27 13:41:35 -0400

    created dummy data

---
+++ dummy-data.sql
@@ -0,0 +1,19 @@
+-- User Table
+insert into Users
+	(LastName, FirstName, Email, Height, BodyWeight, Age, Gender, Ethnicity)
+values
+	('Dirt','Joe','dirtyjoe@dirt.com','70 in','275 lbs','37','Male','Caucasian')	
+;
+-- Medicine Table
+insert into Medicine
+    (MedName, UserId, AmtAvail, LotNum, ExpDate)
+values
+    ('Booster', 1, '13ml', '02f8sd7a23', 'July 30th, 2018')
+;
+-- Shot Info Table
+insert into Shot
+    (MedId, ShotTime, ShotLocation, Notes)
+values
+    (1, '2018/06/30 05:33:47', 'Left Shoulder', 'Allergic to Ampecillin')
+;
+
---
+++ example-queries.sql
@@ -0,0 +1,6 @@
+-- Get all Users
+select * from Users;
+-- Get all Medicine
+select * from Medicine;
+-- Get all Shot
+select * from Shot;
